9144daed2697124885d6cbbb296c85275c85f915
Georges Nomicos <georgesnomicos@gmail.com>
2020-04-06 15:51:09 +0200

    flask application initial commit

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,11 @@
 import os
-
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, jsonify
+from test_fruit_model import fruit_test
+import io
+from PIL import Image, ImageDraw, ImageFont
+import time
+import json
+from random import randint
 
 def net(img):
     return img
@@ -30,7 +35,7 @@ def create_app(test_config=None):
     def updateImage():
         db = get_db()
         image = [request.data]
-        db.execute('UPDATE store SET image=? WHERE id=1', image)
+        db.execute('UPDATE store SET image=? WHERE id=1', image).fetchone()
         db.commit()
         return '', 200
 
@@ -50,21 +55,70 @@ def create_app(test_config=None):
     def static_image():
         return "<img src='/static.jpeg'>"
 
+    @app.route('/camera_status', methods=['GET'])
+    def camera_status():
+        db = get_db()
+        camera_status = db.execute('SELECT image_capture FROM camera1 where id=1').fetchone()[0]
+        print(camera_status)
+        return str(camera_status), 200
+
     @app.route('/test', methods=['GET','POST'])
     def test_image():
+        with open('/Users/georgesnomicos/fridge_net/class_idx.txt') as f:
+            items_dict = json.load(f)
+        items = list(items_dict.keys())
+
         if request.method == "POST":
             button_state = request.form['button']
             if button_state  == 'Forward':
+
+                # Text for img file label (collection of data)
+                item_name = request.form['text']
+                img_idx = str(items_dict[item_name])
+                while len(img_idx) < 3:
+                    img_idx = '0' + img_idx
+                img_name = img_idx + '_' + item_name + '_' + str(randint(0,1000))
+
+                # Changing camera state to image capturing in database
+                db = get_db()
+                camera_status = db.execute('UPDATE camera1 SET image_capture=1')
+                db.commit()
+
+                # Wait for camera status to change
+                time.sleep(3)
+                
+                # Then get image from database 
                 db = get_db()
                 image = db.execute('SELECT image FROM store where id=1').fetchone()[0]
+                prob, item_name = fruit_test(image)
+                stream = io.BytesIO(image)
+                img = Image.open(stream)
 
-                image = net(image)
+                # Collecting images:
+                img.save('/Users/georgesnomicos/fridge_net/fruits-360_dataset/pi_data/' + img_name + '.jpeg')
+
+                d = ImageDraw.Draw(img)
+                font = ImageFont.truetype('/Users/georgesnomicos/Library/Fonts/arial_narrow_7.ttf',50)
+
+                d.text((250,500), item_name[0], fill=(255,0,0),font=font)
+                d.text((250,400), "%s "%(round(prob.item(),2)), fill=(255,0,0),font=font)
+
+                img.save('img.jpeg')
+                with open('img.jpeg','rb') as f:
+                    image = f.read()
 
                 db.execute('UPDATE store SET image=? WHERE id=2', [image])
                 db.commit()
 
-            return redirect(url_for('static_image'))
+                # Change capturing mode
+                db = get_db()
+                camera_status = db.execute('UPDATE camera1 SET image_capture=0')
+                db.commit()
 
-        return render_template('img/img.html')
+            return redirect(url_for('static_image'))
+        return render_template('img/img.html', items=items)
 
     return app
+
+def save_img(file_name):
+    pass
